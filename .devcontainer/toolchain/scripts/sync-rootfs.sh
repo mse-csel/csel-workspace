5a5149a8758c56f34d5938eccc193e80f66178e4
Jacques Supcik <jacques@supcik.net>
2024-03-22 12:01:23 +0000

    imprive rootfs sync

--- .devcontainer/toolchain/scripts/sync-rootfs.sh
+++ .devcontainer/toolchain/scripts/sync-rootfs.sh
@@ -5,7 +5,13 @@ set -o pipefail
 set -o nounset
 # set -o xtrace
 
-rsync -crlpgoD -i \
+rsync -rlpgoD --itemize-changes \
+  --ignore-existing \
+  --exclude=THIS_IS_NOT_YOUR_ROOT_FILESYSTEM \
+  /buildroot/output/target/ \
+  /rootfs/
+  
+rsync -crlpgoD --itemize-changes \
   --exclude=/etc/ssh/** \
   --exclude=/etc/hostname \
   --exclude=/etc/group \
