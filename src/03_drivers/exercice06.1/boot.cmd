fa41cc067a94219e96f26ad8fb736b4b49c896e6
Bastien Veuthey <bastienvty@protonmail.com>
2025-04-02 22:27:34 +0200

    tp1: fix sdcard number in boot.cmd

--- src/03_drivers/exercice06.1/boot.cmd
+++ src/03_drivers/exercice06.1/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
+setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk2p2 rootwait
 
 fatload mmc 0 $kernel_addr_r Image
 #fatload mmc 0 $fdt_addr_r sun50i-h5-nanopi-neo-plus2.dtb
@@ -8,6 +8,5 @@ fdt addr $fdt_addr_r
 fdt resize
 #fdt mknode / mymodule2
 #fdt set /mymodule2 compatible mymodule2
-
 
 booti $kernel_addr_r - $fdt_addr_r
