030b15e1dbe94287b29ad86c23e4d44a1573b626
Vincent Audergon <vincent.audergon@hes-so.ch>
2025-04-11 14:49:59 +0200

    feat: use epoll instead of active wait

---
+++ src/04_system/led-audergon/README.md
@@ -0,0 +1,6 @@
+
+Useful links:
+
+
+
+https://man7.org/linux/man-pages/man2/timerfd_create.2.html
--- src/04_system/led-audergon/src/main.c
+++ src/04_system/led-audergon/src/main.c
@@ -27,9 +27,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/timerfd.h>
 #include <sys/types.h>
+#include <sys/epoll.h>
 #include <time.h>
-#include <unistd.h>
 #include "led.h"
 #define LED "10"
 
@@ -53,27 +54,52 @@ int main(int argc, char* argv[])
     int led = open_led(LED);
     led_toggle(led, true);
 
-    struct timespec t1;
-    clock_gettime(CLOCK_MONOTONIC, &t1);
 
-    int k = 0;
-    while (1) {
-        struct timespec t2;
-        clock_gettime(CLOCK_MONOTONIC, &t2);
+    struct itimerspec spec;
+    memset(&spec, 0, sizeof(spec));
+    spec.it_interval.tv_sec = 5;
+    spec.it_value.tv_sec = 5;
 
-        long delta =
-            (t2.tv_sec - t1.tv_sec) * 1000000000 + (t2.tv_nsec - t1.tv_nsec);
-
-        int toggle = ((k == 0) && (delta >= p1)) | ((k == 1) && (delta >= p2));
-        if (toggle) {
-            t1 = t2;
-            k  = (k + 1) % 2;
-            if (k == 0)
-                led_toggle(led, true);
-            else
-                led_toggle(led, false);
-        }
+    int timerfd = timerfd_create(CLOCK_REALTIME, 0);
+    if (timerfd == -1) {
+        perror("ERROR");
+    }
+    if (timerfd_settime(timerfd, 0, &spec, NULL) == -1) {
+        perror("ERROR");
     }
 
+    int epfd = epoll_create1(0);
+    if (epfd == -1) {
+        perror("epoll_create1");
+        exit(EXIT_FAILURE);
+    }
+
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = timerfd;
+
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, timerfd, &ev) == -1) {
+        perror("ERROR");
+        exit(EXIT_FAILURE);
+    }
+
+    bool led_state = false;
+
+    while (1) {
+        struct epoll_event events[1];
+        int nfds = epoll_wait(epfd, events, 1, -1);
+        if (nfds == -1) {
+            perror("epoll_wait");
+            exit(EXIT_FAILURE);
+        }
+
+        // On lit le timerfd pour réarmer le timer
+        uint64_t expirations;
+        read(timerfd, &expirations, sizeof(expirations));
+        led_toggle(led, led_state);
+        led_state = !led_state;
+        
+    }
+    
     return 0;
 }
