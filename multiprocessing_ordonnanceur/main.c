0fbbe5dffc33fd71981407011771b926396fbc9c
Jonathan Amez-Droz <jonathan.amez-droz@bluewin.ch>
2025-05-12 08:19:07 +0200

    feat:ready for lab part 2

---
+++ multiprocessing_ordonnanceur/main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+int main(int argc, char** argv){
+    printf("hello there\n");
+
+    return 0;
+}
---
+++ multiprocessing_ordonnanceur/src/communication.h
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2018 University of Applied Sciences Western Switzerland / Fribourg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Project: HEIA-FR / HES-SO MSE - MA-CSEL1 Laboratory
+ *
+ * Abstract: System programming -  file system
+ *
+ * Purpose: NanoPi silly status led control system
+ *
+ * Author:  Jonathan Amez-Droz
+ * Date:    11.04.2025
+ */
+
+
+/* Switches pining
+* k1 gpio_a.0-k1 
+* k2 gpio_a.2-k2
+* k3 gpio_a.3-k3
+*/
+#ifndef COMMUNICATION_H
+#define COMMUNICATION_H
+
+
+#endif // COMMUNICATION_H
