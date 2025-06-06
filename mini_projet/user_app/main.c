52b6082a68db3b385075b673d6dab062ce3d1c0d
Jonathan Amez-Droz <jonathan.amez-droz@bluewin.ch>
2025-06-06 17:59:15 +0200

    feat:created menu body

--- mini_projet/user_app/main.c
+++ mini_projet/user_app/main.c
@@ -1,14 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <syslog.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 
 #define COMM_FILE_PATH "/tmp/comm_file"
+typedef enum {
+    STATE_EXIT = 0,
+    STATE_CHANGE_MODE = 1,
+    STATE_INCREASE_FREQUENCY = 2,
+    STATE_DECREASE_FREQUENCY = 3
+} State;
 
 int read_int_from_keyboard(){
     int value = -1;
     printf("Enter an integer: ");
-    while ((scanf("%d", &value) != 1) || (value < 0) || (value > 4)) {
-        printf("Invalid input. Please enter an integer between 0 and 4: ");
+    while ((scanf("%d", &value) != 1) || (value < 0) || (value > 3)) {
+        printf("Invalid input. Please enter an integer between 0 and 3: ");
         while(getchar() != '\n'); // clear the input buffer
     }
     return value;
@@ -30,9 +39,23 @@ int write_file(char* buffer){
     return 0;
 }
 
+void print_menu(){
+    pritnf("-------CSEL - mini projet-------\n");
+    printf("----------user menu-------------\n");
+    printf("Enter a number to choose an option");
+    printf("0: EXIT\n");
+    printf("1: Change mode\n");
+    printf("2: Increase frequency\n");
+    printf("3: Decrease frequency\n");
+}
+
 int main(){
-    int deux = read_int_from_keyboard();
+    State user_input = read_int_from_keyboard();
+    
     while(1){
+        switch (user_input){
+
+        }
 
     }
 
