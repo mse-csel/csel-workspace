5d342f45a7205511af48705e8de032af0a95aa3e
Jonathan Amez-Droz <jonathan.amez-droz@bluewin.ch>
2025-06-06 11:04:46 +0200

    feat:added files for module communication

---
+++ mini_projet/src/module_com.c
@@ -0,0 +1,10 @@
+#include "module_com.h"
+
+
+
+char read_device(void){
+    
+}
+
+
+int write_device(char c);
---
+++ mini_projet/src/module_com.h
@@ -0,0 +1,13 @@
+
+#ifndef MODULE_COM_H
+#define MODULE_COM_H
+
+
+
+#define DEVICE_PATH   "/dev/mini_projet"
+
+char read_device(void);
+int write_device(char c);
+
+
+#endif//MODULE_COM_H
