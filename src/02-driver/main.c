7f12a642e23f3240d96163b0b41177576189ce90
fastium <fastium.pro@proton.me>
2026-04-01 20:55:21 +0200

    chore(driver): setup files and module

---
+++ src/02-driver/main.c
@@ -0,0 +1,18 @@
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/errno.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+
+int main() {
+
+    int fd = open()
+
+
+
+    return 0;
+}
