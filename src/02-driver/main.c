62e059775c65179cc4755856a725effda07bed3d
fastium <fastium.pro@proton.me>
2026-04-02 13:52:30 +0200

    feat(lab03): update main with ex 1

--- src/02-driver/main.c
+++ src/02-driver/main.c
@@ -3,8 +3,6 @@
 #include "exercice/ex1-memory-oriented.c"
 #define MEMORY_ORIENTED
 
-#include "exercice/ex2-charachter-oriented.c"
-// #define CHARACTER_ORIENTED
 
 
 int main() {
@@ -15,13 +13,6 @@ int main() {
     printf("Exercice 1: Memory oriented exercice\n");
     ret = ex_memory_oriented();
 #endif
-
-#ifdef CHARACTER_ORIENTED
-    printf("--------------------------------------\n");
-    printf("Exercice 2: Character oriented exercice\n");
-    ret = ex_character_oriented();
-#endif
-
 
     return ret;
 }
